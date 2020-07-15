6d80b7a0f933a279cec4cf5f0beb885d064be416
Sabato Nocera <nocera.sabato@protonmail.com>
2020-07-15 16:20:43 +0200

    Third data augmentation technique

--- src/script/data_augmentation.py
+++ src/script/data_augmentation.py
@@ -53,29 +53,45 @@ new_tuples = numpy.array([])
 feature_temperature = 0
 feature_heartbeat = 2
 
-range_temperature = 0.803827
-range_heartbeat = 0.643367
+# 5% standard deviation
+# range_temperature = 0.803827
+# range_heartbeat = 0.643367
 
-for tmp_list in grouped_by_user:
-    last_list = []
-    for tmp_array in tmp_list:
-        if len(last_list) == 0:
-            last_list = tmp_array
-            continue
-        if tmp_array[10] == last_list[10]:
-            tmp_new_tuples = tuples_crossover(tmp_array, last_list)
-            new_tuple_one = numpy.array(tmp_new_tuples[0])
-            new_tuple_two = numpy.array(tmp_new_tuples[1])
+# 5% variance
+range_temperature = 13
+range_heartbeat = 8
 
-            new_tuple_one[feature_temperature] += random.uniform(-range_temperature, +range_temperature)
-            new_tuple_one[feature_heartbeat] += random.uniform(-range_heartbeat, +range_heartbeat)
+for i in range(len(dataset)):
+    new_tuple = numpy.array(dataset[i])
 
-            new_tuple_two[feature_temperature] += random.uniform(-range_temperature, +range_temperature)
-            new_tuple_two[feature_heartbeat] += random.uniform(-range_heartbeat, +range_heartbeat)
+    new_tuple[feature_temperature] += random.choice([i for i in range(-range_temperature,+range_temperature) if i not in [-2,-1,0,1,2,2]])
+    new_tuple[feature_heartbeat] += random.choice([i for i in range(-range_heartbeat,+range_heartbeat) if i not in [-1,0,1]])
+    new_tuples = numpy.concatenate((new_tuples, new_tuple), axis=0)
 
-            new_tuples = numpy.concatenate((new_tuples, new_tuple_one), axis=0)
-            new_tuples = numpy.concatenate((new_tuples, new_tuple_two), axis=0)
-        last_list = tmp_array
+    new_tuple[feature_temperature] += random.choice([i for i in range(-range_temperature,+range_temperature) if i not in [-2,-1,0,1,2,2]])
+    new_tuple[feature_heartbeat] += random.choice([i for i in range(-range_heartbeat,+range_heartbeat) if i not in [-1,0,1]])
+    new_tuples = numpy.concatenate((new_tuples, new_tuple), axis=0)
+
+# for tmp_list in grouped_by_user:
+#     last_list = []
+#     for tmp_array in tmp_list:
+#         if len(last_list) == 0:
+#             last_list = tmp_array
+#             continue
+#         if tmp_array[10] == last_list[10]:
+#             tmp_new_tuples = tuples_crossover(tmp_array, last_list)
+#             new_tuple_one = numpy.array(tmp_new_tuples[0])
+#             new_tuple_two = numpy.array(tmp_new_tuples[1])
+#
+#             new_tuple_one[feature_temperature] += random.uniform(-range_temperature, +range_temperature)
+#             new_tuple_one[feature_heartbeat] += random.uniform(-range_heartbeat, +range_heartbeat)
+#
+#             new_tuple_two[feature_temperature] += random.uniform(-range_temperature, +range_temperature)
+#             new_tuple_two[feature_heartbeat] += random.uniform(-range_heartbeat, +range_heartbeat)
+#
+#             new_tuples = numpy.concatenate((new_tuples, new_tuple_one), axis=0)
+#             new_tuples = numpy.concatenate((new_tuples, new_tuple_two), axis=0)
+#         last_list = tmp_array
 
 new_tuples = new_tuples.reshape((int(len(new_tuples) / 11), 11))
 
