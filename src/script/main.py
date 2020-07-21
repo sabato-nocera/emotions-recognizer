ae07bffb0525cccfebc6d8b7dc7215d8c8997371
nocera-sabato <nocera.sabato@gmail.com>
2020-07-21 10:02:28 +0200

    Training cnn+lstm

--- src/script/main.py
+++ src/script/main.py
@@ -41,7 +41,7 @@
 # exec(open("../resnet/resnet_nohumidityds_categoricalcrossentropy_adam_greater_normalization.py").read())
 # exec(open("../resnet/resnet_nohumidityds_categoricalcrossentropy_adam_greatest_normalization.py").read())
 
-exec(open("../cnn+lstm/cnn+lstm_nohumidityds_categoricalcrossentropy_adam.py").read())
+# exec(open("../cnn+lstm/cnn+lstm_nohumidityds_categoricalcrossentropy_adam.py").read())
 exec(open("../cnn+lstm/cnn+lstm_nohumidityds_categoricalcrossentropy_adam_normalized.py").read())
 exec(open("../cnn+lstm/cnn+lstm_nohumidityds_categoricalcrossentropy_adam_smaller_normalization.py").read())
 exec(open("../cnn+lstm/cnn+lstm_nohumiditydsreduced_categoricalcrossentropy_adam.py").read())
