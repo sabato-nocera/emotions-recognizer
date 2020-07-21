dd234773aa442cd43e6cb9bb8a2edaeabd7cfd3b
Sabato Nocera <nocera.sabato@protonmail.com>
2020-07-21 10:12:51 +0200

    Log analysis

--- src/script/log_analyzer.py
+++ src/script/log_analyzer.py
@@ -313,7 +313,7 @@ sys.stdout = output_file
 
 files_content = merge_per_folder("../../logs/past")
 
-print("past-512.64.5KFCV")
+print("past-110.64.10KFCV")
 
 files_content = files_content[:-1]
 files_content = "[" + files_content + "]"
